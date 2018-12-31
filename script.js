1ea777aec11d86d8eb6bf561d20c66a0baafccb7
Kodmit <alexandre.bailly@live.fr>
2018-12-31 17:34:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function kod_generate(){
   var regExp = /\[\[(.*)\]\]/g;
-  var matches = $("div.woocommerce-variation-add-to-cart").first().html().match(regExp);
+  var matches = $("body").html().match(regExp);
 
   if(matches){
     for (var i = 0; i < matches.length; i++) {
@@ -10,7 +10,7 @@ function kod_generate(){
       $.getJSON( "data.json", function( data ) {
         $.each( data, function( key, val ) {
         if(str === key){
-          $("div.woocommerce-variation-add-to-cart").html($("div.woocommerce-variation-add-to-cart").html().replace(str_base,val));
+          $("body").html($("body").html().replace(str_base,val));
           // FOR DEBUG
           //console.log(str_base + " replaced by " + val);
         }
